739b67c24562e3a4e21dc65e928ffbf1b9f2c93f
Tofupaste <abiputra3566@gmail.com>
2024-10-03 23:56:11 +0700

    add desc

--- collect_add_all.dart
+++ collect_add_all.dart
@@ -1,6 +1,6 @@
 void main () {
   var joshikouse = {'aki', 'mari', 'kaori'};
-  joshikouse.addAll({'shiho', 'momoka', 'hajime', 'himari'});
-  joshikouse.add('yori');
+  joshikouse.addAll({'shiho', 'momoka', 'hajime', 'himari'}); // can add multiple elements
+  joshikouse.add('yori'); // just add one element
   print(joshikouse);
 }
